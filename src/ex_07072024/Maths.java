df278410058535485d1e6f081412d72cc962511e
RAHUL <rahuldarade3461@gmail.com>
2024-07-08 17:21:30 +0530

    Initial commit

---
+++ src/ex_07072024/Maths.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+
+public class Maths {
+    public static void main(String[] args) {
+
+        System.out.println("MAth");
+        System.out.println(200+200);
+
+    }
+}
