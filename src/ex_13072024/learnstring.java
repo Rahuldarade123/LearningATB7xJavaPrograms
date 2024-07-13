207edda8448eb7bd2d6fe87339d98f38d6897900
RAHUL <rahuldarade3461@gmail.com>
2024-07-13 21:24:25 +0530

    JAVA

---
+++ src/ex_13072024/learnstring.java
@@ -0,0 +1,15 @@
+package ex_13072024;
+
+import java.util.Locale;
+
+public class learnstring {
+    public static void main(String[] args) {
+        String s3="The Testing Academy";
+        String s4="The Testing Academy";
+       s3.toLowerCase();
+        System.out.println(s3==s4);
+        // it will check whether references are same??
+        System.out.println(s3.equals(s4));
+        // it will check whether strings are same?
+    }
+}
