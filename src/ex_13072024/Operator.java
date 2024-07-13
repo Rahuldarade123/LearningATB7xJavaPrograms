2059403d1b7c7036a269e9124246b91d42071748
RAHUL <rahuldarade3461@gmail.com>
2024-07-13 12:26:06 +0530

    FirsttJAVAprogram

---
+++ src/ex_13072024/Operator.java
@@ -0,0 +1,11 @@
+package ex_13072024;
+
+public class Operator {
+    public static void main(String[] args) {
+        int a=10;
+        int b= 20;
+        System.out.println(a==b);
+    }
+
+
+}
