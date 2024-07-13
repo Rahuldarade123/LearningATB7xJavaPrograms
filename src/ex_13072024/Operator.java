98dfd61fbe9d0d84faea26732d72edb4caca3773
RAHUL <rahuldarade3461@gmail.com>
2024-07-13 13:29:46 +0530

    FirsttJAVAprogram

--- src/ex_13072024/Operator.java
+++ src/ex_13072024/Operator.java
@@ -1,11 +1,12 @@
 package ex_13072024;
 
 public class Operator {
-    public static void main(String[] args) {
-        int a=10;
-        int b= 20;
-        System.out.println(a==b);
-    }
+    public static void main(String[] args) {        int n = 4;
+        for (int i = 1; i <= 10; i++) {
+           // System.out.printf("%d\t%d\t\n", i+"x"+n+"=", n*i);
+            //System.out.println(n*i);
+            System.out.printf("%d X %d=%d \n", n,i,n*i );
 
-
+}
+}
 }
---
+++ src/ex_13072024/tables.java
@@ -0,0 +1,12 @@
+package ex_13072024;
+
+public class tables {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= 10; i++) {
+            // System.out.printf("%d\t%d\t\n", i+"x"+n+"=", n*i);
+            //System.out.println(n*i);
+            System.out.printf("%d * %d=%d \n", n, i, n * i);
+        }
+    }
+}
