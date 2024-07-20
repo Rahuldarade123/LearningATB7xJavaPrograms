ce44a2b27c981b26ef6e9fc896a65d8ac8014354
RAHUL <rahuldarade3461@gmail.com>
2024-07-20 15:27:11 +0530

    JAVA

---
+++ src/ex_19072024/Factorial.java
@@ -0,0 +1,12 @@
+package ex_19072024;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int num=10;
+        long fact=1;
+for(int i=1;i<=num;i++){
+    fact=fact*i;
+}
+System.out.println(fact);
+    }
+}
